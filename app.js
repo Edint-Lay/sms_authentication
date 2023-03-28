ba973cf108495abb0683a1c4cca3e274a1e25a85
Edint-Lay <wngur89@edint.io>
2023-03-28 18:12:21 +0900

    commit 2

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 
 require("dotenv").config();
 
+app.post('/sms', async( req, res) => {
+    const { phoneNumber } = req.body;
+
+    const date = Date.now().toString();
+    const uri = process.env.SERVICE_ID; //서비스 ID
+    const secretKey = process.env.NCP_SECRET_KEY;// Secret Key
+    const accessKey = process.env.NCP_ACCESS_KEY;//Access Key
+})
+
 app.listen(3000, () => {
     console.log(`App Started 3000`)
 })
